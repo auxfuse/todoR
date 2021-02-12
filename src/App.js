c06a585def7801699808f32653f8216c19c3e4fb
auxfuse <aux.fuse@yahoo.com>
2021-02-12 16:44:51 +0000

    Form input accepting and adding values to state

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import TodoList from "./components/TodoList";
 import './App.css';
 
 function App() {
+
     const [inputText, setInputText] = useState('');
+    const [todos, setTodos] = useState([]);
 
     return (
         <div className="App">
             <header>
                 <h1>Todos</h1>
             </header>
-            <Form />
+            <Form
+                inputText={inputText}
+                setInputText={setInputText}
+                todos={todos}
+                setTodos={setTodos}
+            />
             <TodoList />
         </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,42 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({ inputText, setInputText, todos, setTodos }) => {
 
     const inputTextHandler = (e) => {
         console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            {text: inputText, completed: false, id: Math.random()*1000}
+        ]);
+        setInputText("");
     }
 
     return (
         <>
             <form>
-                <input type="text" className="todo-input" onChange={inputTextHandler}/>
-                <button className="todo-button" type="submit">
+                <input
+                    type="text"
+                    className="todo-input"
+                    value={inputText}
+                    onChange={inputTextHandler}
+                />
+                <button
+                    type="submit"
+                    className="todo-button"
+                    onClick={submitTodoHandler}
+                >
                     <i className="fas fa-plus-square" />
                 </button>
                 <div className="select">
-                    <select name="todos" className="filter-todo">
+                    <select
+                        className="filter-todo"
+                        name="todos"
+                    >
                         <option value="all">All</option>
                         <option value="completed">Completed</option>
                         <option value="uncompleted">Uncompleted</option>
